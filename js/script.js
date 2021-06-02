241e263e4d71ed8c6c133e0735fa82ddc6c1d1ff
N-kazu42 <shiotsupapakauaki@gmail.com>
2021-06-03 08:05:50 +0900

    タブレットサイズスライド

--- js/script.js
+++ js/script.js
@@ -10,9 +10,10 @@ var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0
 			];
 		} else {
 			var responsiveImage = [//タブレットサイズ（768px）以下用の画像
-				{ src: './img/img_sp_01.jpg' },
-				{ src: './img/img_sp_02.jpg' },
-				{ src: './img/img_sp_03.jpg' }
+				{ src: './images/slide-image1.jpg'},
+				{ src: './images/slide-image2.jpg'},
+				{ src: './images/slide-image3.jpg'},
+				{ src: './images/slide-image4.jpg'}
 			];
 		}
 
@@ -20,7 +21,7 @@ var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0
 
 $('#slider').vegas({
 		overlay: true,//画像の上に網線やドットのオーバーレイパターン画像を指定。
-		transition: 'blur',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
+		transition: 'flash',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
 		transitionDuration: 2000,//切り替わりのアニメーション時間をミリ秒単位で設定
 		delay: 10000,//スライド間の遅延をミリ秒単位で。
 		animationDuration: 20000,//スライドアニメーション時間をミリ秒単位で設定
